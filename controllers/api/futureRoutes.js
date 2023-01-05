2e8f84f3a4eab203d7f4bdcb6162d2d4f5405420
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-01-05 13:42:45 -0700

    Set up user login and logout and tied trips added and displayed to logged-in user

--- controllers/api/futureRoutes.js
+++ controllers/api/futureRoutes.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const { Future } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
-//router.post('/', withAuth, async (req, res) => {
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newTrip = await Future.create({
       ...req.body,
@@ -17,22 +15,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-  router.delete('/:id', async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
     try {
       const futureData = await Future.destroy({
         where: {
           id: req.params.id,
-        },
-      });
-  
-      /* DELETE TRIP BY ID BY USER
-        const futureData = await Future.destroy({
-        where: {
-          id: req.params.id,
           user_id: req.session.user_id,
         },
-      }); */
+      });
   
       if (!futureData) {
         res.status(404).json({ message: 'No past trip found with this id!' });
