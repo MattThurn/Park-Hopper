27a36535fbeb572cb495f85918964b7ea78fc04f
Matt Thurn <matt.thurn@icloud.com>
2023-01-08 10:57:37 -0700

    cleaned utils/helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,19 +1,18 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
+
     return date.toLocaleDateString();
   },
 
-  // TODO: ARE THESE NEEDED?
+
 
   format_amount: (amount) => {
-    // format large numbers with commas
+
     return parseInt(amount).toLocaleString();
   },
   get_emoji: () => {
     const randomNum = Math.random();
 
-    // Return a random emoji
     if (randomNum > 0.7) {
       return `<span for="img" aria-label="lightbulb">💡</span>`;
     } else if (randomNum > 0.4) {
@@ -22,7 +21,7 @@ module.exports = {
       return `<span for="img" aria-label="gear">⚙️</span>`;
     }
   },
-  // this helper gets a random image for the hero banner
+
   get_image: () => {
     const images = [
       '../../public/images/hero-cacti.png',
