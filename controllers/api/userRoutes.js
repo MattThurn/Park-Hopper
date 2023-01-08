acc32b5a414f1a16c6c408b1926797e6dc676480
Matt Thurn <matt.thurn@icloud.com>
2023-01-08 10:45:22 -0700

    cleaned userRoutes.js and fixed api/index.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,23 +1,17 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-
 router.post('/register', async (req, res) => {
   try {
     console.log(req.body);
 
     const userRegData = await User.create(req.body);
 
-    
     req.session.save(() => {
       req.session.user_id = userRegData.id;
       req.session.logged_in = true;
       res.redirect('/');
     });
-
-
-    
-
   } catch (err) {
     console.error(err);
     res.status(400).json(err);
@@ -54,52 +48,6 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
-/* router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-}); */
-
-// router.post('/login', async (req, res) => {
-//   try {
-//     const userData = await User.findOne({ where: { email: req.body.email } });
-
-//     if (!userData) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password, please try again' });
-//       return;
-//     }
-
-//     const validPassword = await userData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password, please try again' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
-
-//       res.json({ user: userData, message: 'You are now logged in!' });
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
